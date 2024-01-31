621019e7e52e189914e7c794553ef7e6ea77c86f
Nicola Cirillo <nicola.cirillo96@outlook.it>
2024-01-31 14:12:45 +0100

    add folder2conll function

--- easy_parser/text2conll.py
+++ easy_parser/text2conll.py
@@ -1,6 +1,7 @@
 from tqdm import tqdm
 import pandas as pd
 import codecs
+import os
 
 
 from .tagger import Tagger
@@ -33,8 +34,22 @@ def lines2conll(txtfile: str, fileout: str, lang: str, encoding="utf8",
      tagger = Tagger(lang)
      with codecs.open(txtfile, "r", encoding) as filein:
           with codecs.open(fileout, "w", "utf8") as writer:
-            if verbose: id_doc = tqdm(filein, desc="documents parsed:")
+            if verbose: filein = tqdm(filein, desc="documents parsed:")
             for i, line in enumerate(filein):
                 head = f"# newdoc id = {i}\n"
                 conll = tagger.tag_doc(line.strip()) + "\n"
                 writer.write(head  + conll)
+
+def folder2conll(folder: str, fileout: str, lang: str, encoding="utf8", 
+                verbose=False):
+      tagger = Tagger(lang)
+      txt_files = [f for f in os.listdir(folder) if f.endswith(".txt")]
+      if verbose: 
+          txt_files = tqdm(txt_files, desc="documents parsed:")
+      with codecs.open(fileout, "w", "utf8") as writer:
+           for f in txt_files:
+            with codecs.open(f"{folder}/{f}", "r", encoding) as filein:
+                doc = filein.read()
+                head = f"# newdoc id = {f.rsplit('.', 1)}\n"
+                conll = tagger.tag_doc(doc.strip()) + "\n"
+                writer.write(head + conll)
